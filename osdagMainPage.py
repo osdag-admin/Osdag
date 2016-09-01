e30e34a83a2acba2b39f32b5e61e76b520debeea
deepa-chaudhari <dipa.chaudhari@gmail.com>
2016-09-01 11:11:47 +0530

    style: reformat code to PEP 8

--- osdagMainPage.py
+++ osdagMainPage.py
@@ -6,34 +6,30 @@ Created on 31-Mar-2016
 import sys
 from PyQt4 import QtGui
 from ui_OsdagMainPage import Ui_MainWindow
-# from Connections.Shear.Finplate.finPlateMain import launchFinPlateController
 from Connections.Shear.Finplate.finPlateMain import launchFinPlateController
 from Connections.Shear.cleatAngle.cleatAngleMain import launchCleatAngleController
 from Connections.Shear.Endplate.endPlateMain import launchEndPlateController
-
-# from os.path import expanduser                       #enters in home folder
 import os.path
-from PyQt4.QtGui import QFileDialog
-import shutil
-    
+
+
 class OsdagMainWindow(QtGui.QMainWindow):
 
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        listItems = {'Osdagpage':0, 'connectionpage':1, 'tensionpage':2, 'compressionpage':3, 'flexuralpage':4}
-        
+        listItems = {'Osdagpage': 0, 'connectionpage': 1, 'tensionpage': 2, 'compressionpage': 3, 'flexuralpage': 4}
+
         self.ui.myStackedWidget.setCurrentIndex(listItems['Osdagpage'])
-        self.ui.btn_connection.clicked.connect(lambda:self.changePage(listItems['connectionpage'], listItems['Osdagpage']))
+        self.ui.btn_connection.clicked.connect(lambda: self.changePage(listItems['connectionpage'], listItems['Osdagpage']))
         self.ui.myListWidget.currentItemChanged.connect(self.changePage)
         self.ui.btn_start.clicked.connect(self.showConnection)
-        self.ui.btn_beamCol.clicked.connect(self.notavailable)
-        self.ui.btn_compression.clicked.connect(self.notavailable)
-        self.ui.btn_flexural.clicked.connect(self.notavailable)
-        self.ui.btn_gantry.clicked.connect(self.notavailable)
-        self.ui.btn_tension.clicked.connect(self.notavailable)
-        self.ui.btn_plate.clicked.connect(self.notavailable)
+        self.ui.btn_beamCol.clicked.connect(self.unavailable)
+        self.ui.btn_compression.clicked.connect(self.unavailable)
+        self.ui.btn_flexural.clicked.connect(self.unavailable)
+        self.ui.btn_gantry.clicked.connect(self.unavailable)
+        self.ui.btn_tension.clicked.connect(self.unavailable)
+        self.ui.btn_plate.clicked.connect(self.unavailable)
 
     def disableDesignButtons(self):
         self.ui.btn_beamCol.setEnabled(False)
@@ -62,6 +58,7 @@ class OsdagMainWindow(QtGui.QMainWindow):
         self.ui.myStackedWidget.setCurrentIndex(current)
 
     def showConnection(self):
+
         folder = str(QtGui.QFileDialog.getSaveFileName(self, "Select Workspace Directory", "../../Osdag_Workspace", "File folder"))
         if not os.path.exists(folder):
             os.makedirs(folder, 0755)
@@ -83,25 +80,23 @@ class OsdagMainWindow(QtGui.QMainWindow):
             launchEndPlateController(self, folder)
             self.ui.myStackedWidget.setCurrentIndex(0)
             # QtGui.QMessageBox.about(self,"INFO","End plate connection design is coming soon!")
-        
+
         elif self.ui.rdbtn_seat.isChecked():
             QtGui.QMessageBox.about(self, "INFO", "Seated connection design is coming soon!")
-        
+
         else:
             QtGui.QMessageBox.about(self, "INFO", "Please select appropriate connection")
-            
-    def notavailable(self):    
+
+    def unavailable(self):
         QtGui.QMessageBox.about(self, "INFO", "This module is not available in this version.")
-#         self.ui.btn_beamCol.clicked.connect(lambda:self.changePage(listItems['Osdagpage'], listItems['tensionpage']))
-#         self.ui.btn_compression.clicked.connect(lambda:self.changePage(listItems['Osdagpage'], listItems['tensionpage']))
-#         self.ui.btn_flexural.clicked.connect(lambda:self.changePage(listItems['Osdagpage'], listItems['tensionpage']))
-#         self.ui.btn_gantry.clicked.connect(lambda:self.changePage(listItems['Osdagpage'], listItems['tensionpage']))
-#         self.ui.btn_plate.clicked.connect(lambda:self.changePage(listItems['Osdagpage'], listItems['tensionpage']))
-#         self.ui.btn_tension.clicked.connect(lambda:self.changePage(listItems['Osdagpage'], listItems['tensionpage']))
+        # self.ui.btn_beamCol.clicked.connect(lambda:self.changePage(listItems['Osdagpage'], listItems['tensionpage']))
+        # self.ui.btn_compression.clicked.connect(lambda:self.changePage(listItems['Osdagpage'], listItems['tensionpage']))
+        # self.ui.btn_flexural.clicked.connect(lambda:self.changePage(listItems['Osdagpage'], listItems['tensionpage']))
+        # self.ui.btn_gantry.clicked.connect(lambda:self.changePage(listItems['Osdagpage'], listItems['tensionpage']))
+        # self.ui.btn_plate.clicked.connect(lambda:self.changePage(listItems['Osdagpage'], listItems['tensionpage']))
+        # self.ui.btn_tension.clicked.connect(lambda:self.changePage(listItems['Osdagpage'], listItems['tensionpage']))
 
-    
 
-    
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     window = OsdagMainWindow()

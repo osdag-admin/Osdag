a518c2802f4ab8b03368f978b59c6b401460c649
Jayant Patil <jayantpatil289@gmail.com>
2016-09-02 17:54:25 +0530

    feat: changing namespace, documentation

--- Connections/Shear/SeatedAngle/SeatAngleCalc.py
+++ Connections/Shear/SeatedAngle/SeatAngleCalc.py
@@ -17,15 +17,6 @@ import math
 import logging
 import model
 
-'''
-
-Rework namespace usage for model.py functions:
-    model.get_angledata
-    model.get_columndata
-    model.get_beamdata
-
-'''
-
 from PyQt4.Qt import QString
 
 # flag = 1
@@ -79,6 +70,8 @@ def bolt_bearing(bolt_diameter, thickness_plate, f_u):
 
 def SeatAngleConn(inputObj):
 
+    #------------------------------------------------------
+
     connectivity = inputObj['Member']['Connectivity']
     beam_section = inputObj['Member']['BeamSection']
     column_section = inputObj['Member']['ColumnSection']
@@ -90,7 +83,7 @@ def SeatAngleConn(inputObj):
     bolt_diameter = inputObj['Bolt']['Diameter (mm)']
     bolt_type = inputObj['Bolt']['Type']
     bolt_grade = inputObj['Bolt']['Grade']
-    bolt_planes = 1
+    # bolt_planes = 1
 
     # angle_sec = 'ISA 15075'
     # TODO rework angle section name
@@ -104,44 +97,47 @@ def SeatAngleConn(inputObj):
     beam_w_f = float(dict_beam_data[QString("B")])
     beam_R1 = float(dict_beam_data[QString("R1")])
 
-    dictcolumndata = model.get_columndata(column_section)
-    column_f_t = float(dictcolumndata[QString("T")])
-    columnt_f_t = 15
+    dict_column_data = model.get_columndata(column_section)
+    column_f_t = float(dict_column_data[QString("T")])
+    # columnt_f_t = 15
 
-    dictangledata = model.get_angledata(angle_sec)
-    angle_t = float(dictangledata[QString("t")])
+    dict_angle_data = model.get_angledata(angle_sec)
+    angle_t = float(dict_angle_data[QString("t")])
     # angle_t = 12
-    angle_a = float(dictangledata[QString("A")])
-    angle_b = float(dictangledata[QString("B")])
-    angle_ra = float(dictangledata[QString("R1")])
+    angle_a = float(dict_angle_data[QString("A")])
+    angle_b = float(dict_angle_data[QString("B")])
+    angle_ra = float(dict_angle_data[QString("R1")])
 
     safe = True
 
-    ########################################################################
+    # ------------------------------------------------------
     # Bolt design:
 
     bolt_fu = int(bolt_grade) * 100
-    bolt_fy = (bolt_grade - int(bolt_grade)) * bolt_fu;
+    # TODO bolt f_y
+        # confirm python arbitrary precision calcs will not cause issues in below logic
+        # bolt_fy = (bolt_grade - int(bolt_grade)) * bolt_fu;
+        # confirm that fy is not required in calculations
 
     # I: Check for number of bolts -------------------
-    t_thinner = min(beam_w_t.real, angle_t.real);
-    bolt_shear_capacity = bolt_shear(bolt_diameter, bolt_planes, bolt_fu).real;
-    bolt_bearing_capacity = bolt_bearing(bolt_diameter, column_f_t, beam_fu).real;
+    thickness_governing = min(beam_w_t.real, angle_t.real)
+    bolt_shear_capacity = bolt_shear(bolt_diameter, 1, bolt_fu).real
+    bolt_bearing_capacity = bolt_bearing(bolt_diameter, column_f_t, beam_fu).real
 
-    bolt_capacity = min(bolt_shear_capacity, bolt_bearing_capacity);
+    bolt_value = min(bolt_shear_capacity, bolt_bearing_capacity)
 
-    bolts_required = int(shear_force / bolt_capacity) + 1;
+    bolts_required = int(shear_force / bolt_value) + 1
     if bolts_required <= 2:
-        bolts_required = 3;
+        bolts_required = 3
 
     # even number of bolts -- confirm with sir(doubt)
     else:
         if bolts_required % 2 == 1:
-            bolts_required = bolts_required + 1;
+            bolts_required = bolts_required + 1
 
     print "no of bolts required =" + str(bolts_required)
 
-    bolt_group_capacity = bolts_required * bolt_capacity;
+    bolt_group_capacity = bolts_required * bolt_value
     print "bolt group capacity =" + str(bolt_group_capacity)
 
     # Spacing of bolts for web plate -------------------
@@ -153,75 +149,75 @@ def SeatAngleConn(inputObj):
         dia_hole = bolt_diameter + 3
 
         # Minimum/maximum pitch and gauge
-    min_pitch = int(2.5 * bolt_diameter);
-    min_gauge = int(2.5 * bolt_diameter);
+    min_pitch = int(2.5 * bolt_diameter)
+    min_gauge = int(2.5 * bolt_diameter)
 
     if min_pitch % 10 != 0 or min_gauge % 10 != 0:
-        min_pitch = (min_pitch / 10) * 10 + 10;
-        min_gauge = (min_gauge / 10) * 10 + 10;
+        min_pitch = (min_pitch / 10) * 10 + 10
+        min_gauge = (min_gauge / 10) * 10 + 10
     else:
-        min_pitch = min_pitch;
-        min_gauge = min_gauge;
+        min_pitch = min_pitch
+        min_gauge = min_gauge
         # clause 10.2.2 is800
     print "minimum pitch =" + str(min_pitch)
     print "minimum gauge =" + str(min_gauge)
 
-    max_spacing = int(min(100 + 4 * t_thinner, 200));  # clause 10.2.3.3 is800
+    max_spacing = int(min(100 + 4 * thickness_governing, 200))  # clause 10.2.3.3 is800
     print "max spacing=" + str(max_spacing)
 
-    min_edge_dist = int(1.5 * (dia_hole)) + 10;  # 10 mm added than min. value
+    min_edge_dist = int(1.5 * (dia_hole)) + 10  # 10 mm added than min. value
     if min_edge_dist % 10 != 0:
-        min_edge_dist = (min_edge_dist / 10) * 10 + 10;
+        min_edge_dist = (min_edge_dist / 10) * 10 + 10
     else:
-        min_edge_dist = min_edge_dist;
+        min_edge_dist = min_edge_dist
 
-    max_edge_dist = int((12 * t_thinner * math.sqrt(250 / beam_fy)).real) - 1;
+    max_edge_dist = int((12 * thickness_governing * math.sqrt(250 / beam_fy)).real) - 1
 
     # Determine single or double line of bolts
 
-    length_avail = (angle_a - 2 * min_edge_dist);
-    pitch = round(length_avail / (bolts_required - 1), 3);
+    length_avail = (angle_a - 2 * min_edge_dist)
+    pitch = round(length_avail / (bolts_required - 1), 3)
 
     ## Calculation of moment demand
 
-    M1 = bolt_shear_capacity * (20 + min_edge_dist / 2);
+    M1 = bolt_shear_capacity * (20 + min_edge_dist / 2)
     # Single line of bolts
     if pitch >= min_pitch:
-        bolt_line = 1;
-        gauge = 0;
-        bolts_one_line = bolts_required;
-        K = bolts_one_line / 2;
-        M2 = 0;
+        bolt_line = 1
+        gauge = 0
+        bolts_one_line = bolts_required
+        K = bolts_one_line / 2
+        M2 = 0
         if bolts_required % 2 == 0 or bolts_required % 2 != 0:
             for k in range(0, K):
                 M2 = M2 + 2 * (
-                bolt_shear_capacity * ((length_avail / 2 - k * pitch) ** 2 / (length_avail / 2 - k * pitch)));
-            moment_demand = max(M1, M2);
+                bolt_shear_capacity * ((length_avail / 2 - k * pitch) ** 2 / (length_avail / 2 - k * pitch)))
+            moment_demand = max(M1, M2)
             moment_demand = round(moment_demand * 0.001, 3)
 
     # Multi-line of bolts
     if pitch < min_pitch:
-        bolt_line = 2;
+        bolt_line = 2
         if bolts_required % 2 == 0:
-            bolts_one_line = bolts_required / 2;
+            bolts_one_line = bolts_required / 2
         else:
-            bolts_one_line = (bolts_required / 2) + 1;
+            bolts_one_line = (bolts_required / 2) + 1
 
-        pitch = round(length_avail / (bolts_one_line - 1), 3);
-        gauge = min_gauge;
-        M1 = bolt_shear_capacity * (20 + min_edge_dist + gauge / 2);
+        pitch = round(length_avail / (bolts_one_line - 1), 3)
+        gauge = min_gauge
+        M1 = bolt_shear_capacity * (20 + min_edge_dist + gauge / 2)
 
         if pitch >= min_pitch:
-            K = bolts_one_line / 2;
-            M2 = 0;
+            K = bolts_one_line / 2
+            M2 = 0
             if bolts_required % 2 == 0 or bolts_required % 2 != 0:
                 for k in range(0, K):
                     V = length_avail / 2 - k * pitch
-                    H = gauge / 2;
-                    d = math.sqrt(V ** 2 + H ** 2);
-                    M2 = M2 + 2 * (bolt_shear_capacity * (d ** 2 / d));
-                M2 = M2 * 2;
-                moment_demand = max(M1, M2);
+                    H = gauge / 2
+                    d = math.sqrt(V ** 2 + H ** 2)
+                    M2 = M2 + 2 * (bolt_shear_capacity * (d ** 2 / d))
+                M2 = M2 * 2
+                moment_demand = max(M1, M2)
                 moment_demand = round(moment_demand * 0.001, 3)
 
                 # Needs discussion with Sir
@@ -329,7 +325,7 @@ def SeatAngleConn(inputObj):
 
     #     outputObj['Bolt']['shearcapacity'] = bolt_shear_capacity
     #     outputObj['Bolt']['bearingcapacity'] = bolt_bearing_capacity
-    #     outputObj['Bolt']['boltcapacity'] = bolt_capacity
+    #     outputObj['Bolt']['boltcapacity'] = bolt_value
     #     outputObj['Bolt']['numofbolts'] = bolts_required
     #     outputObj['Bolt']['boltgrpcapacity'] = bolt_group_capacity
     #     outputObj['Bolt'][''] = bolts_one_line
@@ -354,7 +350,7 @@ def SeatAngleConn(inputObj):
     outputObj['Bolt']['status'] = safe
     outputObj['Bolt']["Shear Capacity (kN)"] = bolt_shear_capacity
     outputObj['Bolt']["Bearing Capacity (kN)"] = bolt_bearing_capacity
-    outputObj['Bolt']["Capacity Of Bolt (kN)"] = bolt_capacity
+    outputObj['Bolt']["Capacity Of Bolt (kN)"] = bolt_value
     outputObj['Bolt']["Bolt group capacity (kN)"] = bolt_group_capacity
     outputObj['Bolt']["No Of Bolts"] = bolts_required
     outputObj['Bolt']["No.Of Row"] = bolts_one_line

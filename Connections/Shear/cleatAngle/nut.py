7c6ebdd7b79e6fff397480f6422b8aba88d22e4c
Reshmakonjari112 <reshmakonjari112@gmail.com>
2016-09-09 18:11:07 +0530

    feat: small changes

--- Connections/Shear/cleatAngle/nut.py
+++ Connections/Shear/cleatAngle/nut.py
@@ -47,11 +47,11 @@ class Nut(object):
 
     def getPoint(self, theta):
         theta = math.radians(theta)
-        point = self.secOrigin + (self.R * math.cos(theta)) * self.uDir + (self.R * math.sin(theta)) * self.vDir 
+        point = self.secOrigin + (self.R * math.cos(theta)) * self.uDir + (self.R * math.sin(theta)) * self.vDir
         return point
 
     def computeParams(self):
-        
+
         self.vDir = numpy.cross(self.wDir, self.uDir)
         self.a1 = self.getPoint(0)
         self.a2 = self.getPoint(60)
@@ -60,10 +60,9 @@ class Nut(object):
         self.a5 = self.getPoint(240)
         self.a6 = self.getPoint(300)
         self.points = [self.a1, self.a2, self.a3, self.a4, self.a5, self.a6]
-       
-        
+
     def createModel(self):
-        
+
         edges = makeEdgesFromPoints(self.points)
         wire = makeWireFromEdges(edges)
         aFace = makeFaceFromWire(wire)
@@ -73,9 +72,9 @@ class Nut(object):
         anEdgeExplorer = TopExp_Explorer(prism, TopAbs_EDGE)
         while anEdgeExplorer.More():
             aEdge = topods.Edge(anEdgeExplorer.Current())
-            mkFillet.Add(self.T / 17. , aEdge)
+            mkFillet.Add(self.T / 17., aEdge)
             anEdgeExplorer.Next()
-                
+
         prism = mkFillet.Shape()
         cylOrigin = self.secOrigin
         # cylOrigin = self.secOrigin + self.T * self.wDir
@@ -83,14 +82,13 @@ class Nut(object):
         # outerCyl = BRepPrimAPI_MakeCylinder(gp_Ax2(getGpPt(cylOrigin), getGpDir(self.wDir)), self.r2, self.H).Shape()
         # nutBody = BRepAlgoAPI_Fuse(prism, outerCyl).Shape()
         # my_cyl = BRepPrimAPI_MakeCylinder(9.0, 6.0).Shape()
-        # result_shape = BRepAlgoAPI_Cut(nutBody, innerCyl).Shape() 
-        result_shape = BRepAlgoAPI_Cut(prism, innerCyl).Shape() 
-        
-        
+        # result_shape = BRepAlgoAPI_Cut(nutBody, innerCyl).Shape()
+        result_shape = BRepAlgoAPI_Cut(prism, innerCyl).Shape()
+
 #         self.secOrigin = gp_Pnt(0 , 0 , 0)
 #         neckNormal = gp_DZ()
 #         # Threading : Create Surfaces
-# 
+#
 #         nutAx2_bis = gp_Ax3(self.secOrigin , neckNormal)
 #         aCyl1 = Geom_CylindricalSurface(nutAx2_bis , self.T * 0.99)
 #         aCyl2 = Geom_CylindricalSurface(nutAx2_bis , self.T * 1.05)
@@ -98,7 +96,7 @@ class Nut(object):
 #         aCyl1_handle = aCyl1.GetHandle()
 #         aCyl2_handle = aCyl2.GetHandle()
 #         #aCyl3_handle = aCyl3.GetHandle()
-#         
+#
 #         # Threading : Define 2D Curves
 #         aPnt = gp_Pnt2d(2. * math.pi , self.H / 2.)
 #         aDir = gp_Dir2d(2. * math.pi , self.H / 4.)
@@ -116,9 +114,9 @@ class Nut(object):
 #         anEllipsePnt1 = anEllipse1.Value(0)
 #         anEllipsePnt2 = anEllipse1.Value(math.pi)
 #         aSegment = GCE2d_MakeSegment(anEllipsePnt1 , anEllipsePnt2)
-#         
+#
 #         # Threading : Build Edges and Wires
-# 
+#
 #         aEdge1OnSurf1 = BRepBuilderAPI_MakeEdge( aArc1_handle , aCyl1_handle)
 #         aEdge2OnSurf1 = BRepBuilderAPI_MakeEdge( aSegment.Value() , aCyl1_handle)
 #         aEdge1OnSurf2 = BRepBuilderAPI_MakeEdge( aArc2_handle , aCyl2_handle)
@@ -128,25 +126,22 @@ class Nut(object):
 #         threadingWire2 = BRepBuilderAPI_MakeWire(aEdge1OnSurf2.Edge() , aEdge2OnSurf2.Edge())#aEdge3OnSurf2.Edge())
 #         BRepLib.breplib.BuildCurves3d(threadingWire1.Shape())
 #         BRepLib.breplib.BuildCurves3d(threadingWire2.Shape())
-#         
+#
 #         # Create Threading
-# 
+#
 #         aTool = BRepOffsetAPI_ThruSections(True)
 #         aTool.AddWire(threadingWire1.Wire())
 #         aTool.AddWire(threadingWire2.Wire())
 #         aTool.CheckCompatibility(False)
 #         myThreading = aTool.Shape()
-#         
+#
 #         #Building the resulting compound
-#         
+#
 #         aRes = TopoDS_Compound()
 #         aBuilder = BRep.BRep_Builder()
 #         aBuilder.MakeCompound(aRes)
 #         aBuilder.Add(aRes, result_shape)
 #         aBuilder.Add(aRes, myThreading)
-#         final_shape = BRepAlgoAPI_Cut(result_shape, myThreading).Shape() 
+#         final_shape = BRepAlgoAPI_Cut(result_shape, myThreading).Shape()
 
-        
         return result_shape
-    
-            

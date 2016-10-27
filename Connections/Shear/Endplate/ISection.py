8eccf9f68734c59dbd6898d13b09af82cca2d059
Reshmakonjari112 <reshmakonjari112@gmail.com>
2016-10-27 18:06:53 +0530

    feat: conventional name changed to PEP8

--- Connections/Shear/Endplate/ISection.py
+++ Connections/Shear/Endplate/ISection.py
@@ -41,7 +41,7 @@ class ISection(object):
                                                     
     ''' 
     
-    def __init__(self, B, T, D, t, R1, R2, alpha, length, notchObj):        
+    def __init__(self, B, T, D, t, R1, R2, alpha, length, notch_obj):
         self.B = B
         self.T = T 
         self.D = D
@@ -51,58 +51,58 @@ class ISection(object):
         self.alpha = alpha
         self.length = length
         self.clearDist = 20
-        self.notchObj = notchObj
-        self.secOrigin = numpy.array([0, 0, 0])
+        self.notch_obj = notch_obj
+        self.sec_origin = numpy.array([0, 0, 0])
         self.uDir = numpy.array([1.0, 0, 0])
         self.wDir = numpy.array([0.0, 0, 1.0])
         
-        self.computeParams()
+        self.compute_params()
     
-    def place(self, secOrigin, uDir, wDir):
-        self.secOrigin = secOrigin
+    def place(self, sec_origin, uDir, wDir):
+        self.sec_origin = sec_origin
         self.uDir = uDir
         self.wDir = wDir        
-        self.computeParams()
+        self.compute_params()
         
-    def computeParams(self):
+    def compute_params(self):
         self.vDir = numpy.cross(self.wDir, self.uDir)
-        self.a1 = self.secOrigin + (self.t / 2.0) * self.uDir + ((self.D / 2.0) - self.T) * self.vDir
-        self.b1 = self.secOrigin + (self.B / 2.0) * self.uDir + ((self.D / 2.0) - self.T) * self.vDir   
-        self.c1 = self.secOrigin + (self.B / 2.0) * self.uDir + (self.D / 2.0) * self.vDir
-        self.a2 = self.secOrigin + (-self.t / 2.0) * self.uDir + ((self.D / 2.0) - self.T) * self.vDir
-        self.b2 = self.secOrigin + (-self.B / 2.0) * self.uDir + ((self.D / 2.0) - self.T) * self.vDir   
-        self.c2 = self.secOrigin + (-self.B / 2.0) * self.uDir + (self.D / 2.0) * self.vDir
-        self.a3 = self.secOrigin + (-self.t / 2.0) * self.uDir + -((self.D / 2.0) - self.T) * self.vDir
-        self.b3 = self.secOrigin + (-self.B / 2.0) * self.uDir + -((self.D / 2.0) - self.T) * self.vDir   
-        self.c3 = self.secOrigin + (-self.B / 2.0) * self.uDir + -(self.D / 2.0) * self.vDir
-        self.a4 = self.secOrigin + (self.t / 2.0) * self.uDir + -((self.D / 2.0) - self.T) * self.vDir
-        self.b4 = self.secOrigin + (self.B / 2.0) * self.uDir + -((self.D / 2.0) - self.T) * self.vDir   
-        self.c4 = self.secOrigin + (self.B / 2.0) * self.uDir + -(self.D / 2.0) * self.vDir
+        self.a1 = self.sec_origin + (self.t / 2.0) * self.uDir + ((self.D / 2.0) - self.T) * self.vDir
+        self.b1 = self.sec_origin + (self.B / 2.0) * self.uDir + ((self.D / 2.0) - self.T) * self.vDir
+        self.c1 = self.sec_origin + (self.B / 2.0) * self.uDir + (self.D / 2.0) * self.vDir
+        self.a2 = self.sec_origin + (-self.t / 2.0) * self.uDir + ((self.D / 2.0) - self.T) * self.vDir
+        self.b2 = self.sec_origin + (-self.B / 2.0) * self.uDir + ((self.D / 2.0) - self.T) * self.vDir
+        self.c2 = self.sec_origin + (-self.B / 2.0) * self.uDir + (self.D / 2.0) * self.vDir
+        self.a3 = self.sec_origin + (-self.t / 2.0) * self.uDir + -((self.D / 2.0) - self.T) * self.vDir
+        self.b3 = self.sec_origin + (-self.B / 2.0) * self.uDir + -((self.D / 2.0) - self.T) * self.vDir
+        self.c3 = self.sec_origin + (-self.B / 2.0) * self.uDir + -(self.D / 2.0) * self.vDir
+        self.a4 = self.sec_origin + (self.t / 2.0) * self.uDir + -((self.D / 2.0) - self.T) * self.vDir
+        self.b4 = self.sec_origin + (self.B / 2.0) * self.uDir + -((self.D / 2.0) - self.T) * self.vDir
+        self.c4 = self.sec_origin + (self.B / 2.0) * self.uDir + -(self.D / 2.0) * self.vDir
         self.points = [self.a1, self.b1, self.c1,
                        self.c2, self.b2, self.a2,
                        self.a3, self.b3, self.c3,
                        self.c4, self.b4, self.a4]
         # self.points = [[0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0]]
 
-    def createModel(self):
+    def create_model(self):
         
-        edges = makeEdgesFromPoints(self.points)
-        wire = makeWireFromEdges(edges)
-        aFace = makeFaceFromWire(wire)
-        extrudeDir = self.length * self.wDir  # extrudeDir is a numpy array
-        prism = makePrismFromFace(aFace, extrudeDir)
+        edges = make_edges_from_points(self.points)
+        wire = make_wire_from_edges(edges)
+        aFace = make_face_from_wire(wire)
+        extrude_dir = self.length * self.wDir  # extrude_dir is a numpy array
+        prism = make_prism_from_face(aFace, extrude_dir)
         
-        if self.notchObj != None:
+        if self.notch_obj != None:
             uDir = numpy.array([-1.0, 0.0, 0])
             wDir = numpy.array([0.0, 1.0, 0.0])
-            shiftOri = self.D / 2.0 * self.vDir + self.notchObj.width / 2.0 * self.wDir + self.B / 2.0 * -self.uDir  # + self.notchObj.width* self.wDir + self.T/2.0 * -self.uDir
-            origin2 = self.secOrigin + shiftOri
+            shift_origin = self.D / 2.0 * self.vDir + self.notch_obj.width / 2.0 * self.wDir + self.B / 2.0 * -self.uDir  # + self.notch_obj.width* self.wDir + self.T/2.0 * -self.uDir
+            origin2 = self.sec_origin + shift_origin
              
-            self.notchObj.place(origin2, uDir, wDir)
-            
-            notchModel = self.notchObj.createModel()
-            prism = BRepAlgoAPI_Cut(prism, notchModel).Shape() 
+            self.notch_obj.place(origin2, uDir, wDir)
+
+            notch_model = self.notch_obj.create_model()
+            prism = BRepAlgoAPI_Cut(prism, notch_model).Shape()
         
         return prism
         
-        + self.notchObj.height / 2.0 * self.uDir
+        + self.notch_obj.height / 2.0 * self.uDir

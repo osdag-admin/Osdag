4a56bcec1315b451643a4ea65a9039496f28cf4b
Reshmakonjari112 <reshmakonjari112@gmail.com>
2016-11-08 15:09:29 +0530

    feat: conventional name changed to PEP8

--- Connections/Shear/cleatAngle/colFlangeBeamWebConnectivity.py
+++ Connections/Shear/cleatAngle/colFlangeBeamWebConnectivity.py
@@ -101,23 +101,23 @@ class ColFlangeBeamWeb(object):
     def get_models(self):
         '''Returning 3D models
         '''
-        return [self.columnModel, self.beamModel, self.angleModel] + self.nut_bolt_array.getModels()
+        return [self.columnModel, self.beamModel, self.angleModel] + self.nut_bolt_array.get_models()
              
     def get_nutboltmodels(self):
         
-        return self.nut_bolt_array.getModels()
+        return self.nut_bolt_array.get_models()
         # return self.nut_bolt_array.getboltModels()
          
     def get_beam_model(self):
         final_beam = self.beamModel
-        nut_bolt_list = self.nut_bolt_array.getModels()
+        nut_bolt_list = self.nut_bolt_array.get_models()
         for bolt in nut_bolt_list[0:(len(nut_bolt_list) // 2)]:
             final_beam = BRepAlgoAPI_Cut(final_beam, bolt).Shape()
         return final_beam
     
     def get_column_model(self):
         final_beam = self.columnModel
-        nut_bolt_list = self.nut_bolt_array.getModels()
+        nut_bolt_list = self.nut_bolt_array.get_models()
         for bolt in nut_bolt_list[:]:
             final_beam = BRepAlgoAPI_Cut(final_beam, bolt).Shape()
         return final_beam

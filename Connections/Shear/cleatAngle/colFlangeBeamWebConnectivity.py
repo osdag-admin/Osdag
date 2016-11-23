3c0d5b3dcb216f6e78f764095ca7b1bdc2dbc182
Reshmakonjari112 <reshmakonjari112@gmail.com>
2016-11-23 11:43:36 +0530

    fix: update get_models to get_model for nutbolt

--- Connections/Shear/cleatAngle/colFlangeBeamWebConnectivity.py
+++ Connections/Shear/cleatAngle/colFlangeBeamWebConnectivity.py
@@ -101,23 +101,23 @@ class ColFlangeBeamWeb(object):
     def get_models(self):
         '''Returning 3D models
         '''
-        return [self.columnModel, self.beamModel, self.angleModel] + self.nut_bolt_array.get_models()
+        return [self.columnModel, self.beamModel, self.angleModel] + self.nut_bolt_array.get_model()
              
     def get_nutboltmodels(self):
         
-        return self.nut_bolt_array.get_models()
+        return self.nut_bolt_array.get_model()
         # return self.nut_bolt_array.getboltModels()
          
     def get_beam_model(self):
         final_beam = self.beamModel
-        nut_bolt_list = self.nut_bolt_array.get_models()
+        nut_bolt_list = self.nut_bolt_array.get_model()
         for bolt in nut_bolt_list[0:(len(nut_bolt_list) // 2)]:
             final_beam = BRepAlgoAPI_Cut(final_beam, bolt).Shape()
         return final_beam
     
     def get_column_model(self):
         final_beam = self.columnModel
-        nut_bolt_list = self.nut_bolt_array.get_models()
+        nut_bolt_list = self.nut_bolt_array.get_model()
         for bolt in nut_bolt_list[:]:
             final_beam = BRepAlgoAPI_Cut(final_beam, bolt).Shape()
         return final_beam

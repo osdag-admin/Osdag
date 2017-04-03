93897c144589db1abe72a53fdd280696ab0ca960
Reshmakonjari112 <reshmakonjari112@gmail.com>
2017-04-03 11:35:13 +0530

    test: add a line

--- Connections/Shear/cleatAngle/cleatAngleMain.py
+++ Connections/Shear/cleatAngle/cleatAngleMain.py
@@ -1358,7 +1358,7 @@ class MainController(QMainWindow):
         self.commLogicObj = CommonDesignLogic(self.alist[0], self.alist[1], self.alist[2], self.alist[3], self.alist[4],
                                               self.alist[5], self.alist[6],
                                               self.alist[7], self.alist[8], self.alist[9], self.alist[10], self.display, self.folder, self.connection)
-        
+
         self.resultObj = self.commLogicObj.resultObj
         alist = self.resultObj.values()
 

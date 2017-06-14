140aa41c3053f82c36d206d4b6e4f1ba5100a15f
Danish023 <danishdyp@gmail.com>
2017-06-14 18:24:21 +0530

    fix: add condition for resolving exit error for design profile window

--- Connections/Shear/Endplate/endPlateMain.py
+++ Connections/Shear/Endplate/endPlateMain.py
@@ -237,9 +237,14 @@ class MyPopupDialog(QDialog):
 
         self.ui.lbl_browse.clear()
         filename, _ = QFileDialog.getOpenFileName(self, 'Open File', " ", 'Images (*.png *.svg *.jpg)', None, QFileDialog.DontUseNativeDialog)
-        base = os.path.basename(str(filename))
-        lblwidget.setText(base)
-        self.desired_location(filename)
+        flag = True
+        if filename == '':
+            flag = False
+            return flag
+        else:
+            base = os.path.basename(str(filename))
+            lblwidget.setText(base)
+            self.desired_location(filename)
 
         return str(filename)
 
@@ -1925,7 +1930,7 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     module_setup()
     # workspace_folder_path, _ = QFileDialog.getSaveFileName(caption='Select Workspace Directory', directory="F:\Osdag_workspace")
-    workspace_folder_path = "D:\Osdag_Workspace\endplate"
+    workspace_folder_path = "F:\Osdag_Workspace\endplate"
     if not os.path.exists(workspace_folder_path):
         os.mkdir(workspace_folder_path, 0755)
     image_folder_path = os.path.join(workspace_folder_path, 'images_html')

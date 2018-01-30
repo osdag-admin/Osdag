0f0e2d652ffa17de843b24583f9c7ea408346633
Siddhesh Chavan <chavan.vjti@gmail.com>
2018-01-30 14:27:07 +0530

    Add: Plate details

--- Connections/Moment/ExtendedEndPlate/extendedBothWays.py
+++ Connections/Moment/ExtendedEndPlate/extendedBothWays.py
@@ -10,12 +10,11 @@ import copy
 class ExtendedBothWays(object):
 
     # def __init__(self, beam, Fweld, Wweld, plate, nut_bolt_array):
-    def __init__(self, beamLeft, beamRight):
+    def __init__(self, beamLeft, beamRight, plateLeft, plateRight):
         self.beamLeft = beamLeft
         self.beamRight = beamRight
-        # self.Fweld = Fweld
-        # self.Wweld = Wweld
-        # self.plate = plate
+        self.plateLeft = plateLeft
+        self.plateRight = plateRight
         # self.nut_bolt_array = nut_bolt_array
         # self.beamModel = None
         # self.weldModelF = None
@@ -25,13 +24,16 @@ class ExtendedBothWays(object):
     def create_3DModel(self):
         self.createBeamLGeometry()
         self.createBeamRGeometry()
-        # self.createFlangeWeldGeometry()
+        self.createPlateLGeometry()
+        self.createPlateRGeometry()
         # self.createWebWeldGeometry()
         # self.create_nut_bolt_array()
 
         #call for create_model from Components
         self.beamLModel = self.beamLeft.create_model()
         self.beamRModel = self.beamRight.create_model()
+        self.plateLModel = self.plateLeft.create_model()
+        self.plateRModel = self.plateRight.create_model()
         # self.plateModel = self.plate.create_model()
         # self.weldModelF = self.Fweld.create_model()
         # self.weldModelW = self.Wweld.create_model()
@@ -49,8 +51,29 @@ class ExtendedBothWays(object):
         beamR_vDir = numpy.array([0.0, 1.0, 0.0])
         self.beamRight.place(beamOriginR, beamR_uDir, beamR_vDir)
 
-    def createPlateGeometry(self):
-        pass
+    def createPlateLGeometry(self):
+        plateOriginL = numpy.array([0.0, 800.0,0.0])
+        plateL_uDir = numpy.array([1.0, 0.0, 0.0])
+        plateL_vDir = numpy.array([0.0, 1.0, 0.0])
+        self.plateLeft.place(plateOriginL, plateL_uDir, plateL_vDir)
+
+    def createPlateRGeometry(self):
+        plateOriginR = numpy.array([0.0, 824.0, 0.0])
+        plateR_uDir = numpy.array([1.0, 0.0, 0.0])
+        plateR_vDir = numpy.array([0.0, 1.0, 0.0])
+        self.plateLeft.place(plateOriginR, plateR_uDir, plateR_vDir)
+
+    def get_beamLModel(self):
+        return self.beamLModel
+
+    def get_beamRModel(self):
+        return self.beamRModel
+
+    def get_plateLModel(self):
+        return self.plateLModel
+
+    def get_plateRModel(self):
+        return self.plateRModel
 
     def createFlangeWeldGeometry(self):
         pass
@@ -61,8 +84,4 @@ class ExtendedBothWays(object):
     def create_nut_bolt_array(self):
         pass
 
-    def get_beamLModel(self):
-        return self.beamLModel
 
-    def get_beamRModel(self):
-        return self.beamRModel

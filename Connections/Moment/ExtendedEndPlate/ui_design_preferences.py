a17ae5b1758a6b1b856005ae9f1fcf7715cbe609
Reshmakonjari112 <reshmakonjari112@gmail.com>
2018-04-25 17:12:19 +0530

    Fix: add table no. for weld

--- Connections/Moment/ExtendedEndPlate/ui_design_preferences.py
+++ Connections/Moment/ExtendedEndPlate/ui_design_preferences.py
@@ -8,7 +8,7 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_Dialog(object):
+class Ui_DesignPreference(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(808, 519)
@@ -527,7 +527,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
+    ui = Ui_DesignPreference()
     ui.setupUi(Dialog)
     Dialog.show()
     sys.exit(app.exec_())

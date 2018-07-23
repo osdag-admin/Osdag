58a1f93d8e9ca18b91998bb2fb9e5f3be0cc1aba
ajmalbabums <ajmalbabums@gmail.com>
2018-07-23 14:45:04 +0530

    fix: Check for end plate height in End plate connection

--- Connections/Shear/Endplate/endPlateCalc.py
+++ Connections/Shear/Endplate/endPlateCalc.py
@@ -579,18 +579,17 @@ def end_connection(ui_obj):
                     logger.error(": Calculated width of the end plate exceeds the width of the column")
                     logger.warning(": Minimum end plate width is %2.2f" % (min_end_plate_w))
 
-        # TODO: Check added by Danish Ansari --> Check the functioning (@ Ajmal Babu)
         if end_plate_l < (0.6 * beam_depth):
             design_check = False
             logger.error(": The height of end plate is less than the minimum required height")
-            logger.warning(": The minimum required height of end plate is %2.2f" % min_end_plate_l)
-            logger.info(": Increase the height of end plate or choose a beam section of smaller depth")
+            logger.warning(": The minimum required height of end plate is %2.2f" % (0.6 * beam_depth))
+            logger.info(": Increase the height of end plate")
 
         if end_plate_l > max_end_plate_l:
             design_check = False
             logger.error(": The height of end plate exceeds the maximum allowed value")
             logger.warning(": The maximum allowed height of end plate is %2.2f" % max_end_plate_l)
-            logger.info(": Decrease the height of end plate or choose a beam section of greater depth")
+            logger.info(": Decrease the height of end plate")
 
     else:
 
@@ -602,6 +601,7 @@ def end_connection(ui_obj):
         pitch = avbl_length / (no_row - 1)
         end_dist = min_end_dist
         edge_dist = min_edge_dist
+        min_end_plate_l = 0.6 * beam_depth
         max_end_plate_l = beam_depth - 2 * (beam_f_t + beam_R1)
         test = True
         if end_plate_l > max_end_plate_l:
@@ -693,18 +693,17 @@ def end_connection(ui_obj):
             end_dist = (end_plate_l - (no_row - 1) * pitch) / 2
 
 # ############################ check end plate height ##########################################
-        # TODO: Check added by Danish Ansari --> Check the functioning (@ Ajmal Babu)
-        if end_plate_l < (0.6 * beam_depth):
+        if end_plate_l < min_end_plate_l:
             design_check = False
             logger.error(": The height of end plate is less than the minimum required height")
             logger.warning(": The minimum required height of end plate is %2.2f" % min_end_plate_l)
-            logger.info(": Increase the height of end plate or choose a beam section of smaller depth")
+            logger.info(": Increase the height of end plate ")
 
         if end_plate_l > max_end_plate_l:
             design_check = False
             logger.error(": The height of end plate exceeds the maximum allowed value")
             logger.warning(": The maximum allowed height of end plate is %2.2f" % max_end_plate_l)
-            logger.info(": Decrease the height of end plate or choose a beam section of larger depth")
+            logger.info(": Decrease the height of end plate ")
 
 
             # ############################ check end plate width ##########################################

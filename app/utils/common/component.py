4b869e5b41534ec945e9970a9232288a4a09831c
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2018-11-01 15:40:26 +0530

    Add default values in constructors for parameters in Component, Bolt, Nut, Section, Beam, Column, Plate and Angle classes

--- app/utils/common/component.py
+++ app/utils/common/component.py
@@ -4,31 +4,31 @@ import sqlite3
 
 class Component(object):
 
-    def __init__(self):
-        self.material = Material()
+    def __init__(self, material=Material()):
+        self.material = material
         self.path_to_database = "../../databases/Intg_osdag.sqlite"
 
 
 class Bolt(Component):
 
-    def __init__(self):
-        self.grade = 0.0
-        self.diameter = 0.0
-        self.bolt_type = ""
-        self.length = 0.0
-        super(Bolt, self).__init__()
+    def __init__(self, grade=0.0, diameter=0.0, bolt_type="", length=0.0, material=Material()):
+        self.grade = grade
+        self.diameter = diameter
+        self.bolt_type = bolt_type
+        self.length = length
+        super(Bolt, self).__init__(material)
 
 
 class Nut(Component):
 
-    def __init__(self):
-        self.diameter = 0.0
-        super(Nut, self).__init__()
+    def __init__(self, diameter=0.0, material=Material()):
+        self.diameter = diameter
+        super(Nut, self).__init__(material)
 
 
 class Section(Component):
 
-    def __init__(self, designation):
+    def __init__(self, designation, material=Material()):
         self.designation = designation
         self.depth = 0.0
         self.flange_width = 0.0
@@ -36,7 +36,7 @@ class Section(Component):
         self.flange_thickness = 0.0
         self.root_radius = 0.0
         self.toe_radius = 0.0
-        super(Section, self).__init__()
+        super(Section, self).__init__(material)
 
     def connect_to_database_update_other_attributes(self, table, designation):
         conn = sqlite3.connect(self.path_to_database)
@@ -57,38 +57,38 @@ class Section(Component):
 
 class Beam(Section):
 
-    def __init__(self, designation):
-        super(Beam, self).__init__(designation)
+    def __init__(self, designation, material=Material()):
+        super(Beam, self).__init__(designation, material)
         self.connect_to_database_update_other_attributes("Beams", designation)
 
 
 class Column(Section):
 
-    def __init__(self, designation):
-        super(Column, self).__init__(designation)
+    def __init__(self, designation, material=Material()):
+        super(Column, self).__init__(designation, material)
         self.connect_to_database_update_other_attributes("Columns", designation)
 
 
 class Weld(Component):
 
-    def __init__(self):
-        self.size = 0.0
-        self.length = 0.0
-        super(Weld, self).__init__()
+    def __init__(self, size=0.0, length=0.0, material=Material()):
+        self.size = size
+        self.length = length
+        super(Weld, self).__init__(material)
 
 
 class Plate(Component):
 
-    def __init__(self):
-        self.thickness = 0.0
-        self.height = 0.0
-        self.width = 0.0
-        super(Plate, self).__init__()
+    def __init__(self, thickness=0.0, height=0.0, width=0.0, material=Material()):
+        self.thickness = thickness
+        self.height = height
+        self.width = width
+        super(Plate, self).__init__(material)
 
 
 class Angle(Component):
 
-    def __init__(self, designation):
+    def __init__(self, designation, material=Material()):
         self.designation = designation
 
         self.leg_a_length = 0.0
@@ -98,7 +98,7 @@ class Angle(Component):
         self.connect_to_database_update_other_attributes(designation)
 
         self.length = 0.0
-        super(Angle, self).__init__()
+        super(Angle, self).__init__(material)
 
     def connect_to_database_update_other_attributes(self, designation):
         conn = sqlite3.connect(self.path_to_database)

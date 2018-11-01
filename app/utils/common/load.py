faf67e6c3021d459954ae85fa2ccf10945ef4ee7
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2018-11-01 16:24:10 +0530

    Remove extra space

--- app/utils/common/load.py
+++ app/utils/common/load.py
@@ -1,4 +1,4 @@
-class Load (object):
+class Load(object):
 
     def __init__(self, axial_force=0.0, shear_force=0.0, moment=0.0):
         self.axial_force = axial_force
--- app/utils/common/material.py
+++ app/utils/common/material.py
@@ -1,4 +1,4 @@
-class Material (object):
+class Material(object):
 
     def __init__(self, fy=0.0, fu=0.0):
         self.fy = fy

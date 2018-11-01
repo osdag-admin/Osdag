6b5d6dbe6695de3684a2288ecd7089736533afde
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2018-11-01 14:57:15 +0530

    Add default values in constructor for parameters in Load class

--- app/utils/common/load.py
+++ app/utils/common/load.py
@@ -1,6 +1,6 @@
 class Load (object):
 
-    def __init__(self):
-        self.axial_force = 0.0
-        self.shear_force = 0.0
-        self.moment = 0.0
+    def __init__(self, axial_force=0.0, shear_force=0.0, moment=0.0):
+        self.axial_force = axial_force
+        self.shear_force = shear_force
+        self.moment = moment

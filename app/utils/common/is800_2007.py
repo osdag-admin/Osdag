8a527b394df5e8a1f093f43a747293b17e2bced2
Ajmal Babu M S <ajmalbabums@gmail.com>
2018-11-08 15:31:43 +0530

    feat: Add section outline

--- app/utils/common/is800_2007.py
+++ app/utils/common/is800_2007.py
@@ -1,6 +1,62 @@
 class IS800_2007(object):
 
-    @staticmethod
+# =============================================================
+#    SECTION  1     GENERAL
+# =============================================================
+#    SECTION  2     MATERIALS
+# =============================================================
+#    SECTION  3     GENERAL DESIGN REQUIREMENTS
+# =============================================================
+#    SECTION  4     METHODS OF STRUCTURAL ANALYSIS
+# =============================================================
+#    SECTION  5     LIMIT STATE DESIGN
+# =============================================================
+#    SECTION  6     DESIGN OF TENSION MEMBERS
+# =============================================================
+#    SECTION  7     DESIGN OF COMPRESS1ON MEMBERS
+# =============================================================
+#    SECTION  8     DESIGN OF MEMBERS SUBJECTED TO BENDING
+# =============================================================
+#    SECTION  9     MEMBER SUBJECTED TO COMBINED FORCES
+# =============================================================
+#    SECTION  10    CONNECTIONS
+# =============================================================
+#    SECTION  11    WORKING STRESS DESIGN
+# =============================================================
+#    SECTION  12    DESIGN AND DETAILING FOR EARTHQUAKE
+# =============================================================
+#    SECTION  13    FATIGUE
+# =============================================================
+#    SECTION  14    DESIGN ASSISTED BY TESTING
+# =============================================================
+#    SECTION  15    DURABILITY
+# =============================================================
+#    SECTION  16    FIRE RESISTANCE
+# =============================================================
+#    SECTION  17    FABRICATION AND ERECTION
+# =============================================================
+#    ANNEX  A       LIST OF REFERRED INDIAN STANDARDS
+# =============================================================
+#    ANNEX  B       ANALYSIS AND DESIGN METHODS
+# =============================================================
+#    ANNEX  C       DESIGN AGAINST FLOOR VIBRATION
+# =============================================================
+#    ANNEX  D       DETERMINATION OF EFFECTIVE LENGTH OF COLUMNS
+# =============================================================
+#    ANNEX  E       ELASTIC LATERAL TORSIONAL BUCKLING
+# =============================================================
+#    ANNEX  F       CONNECTIONS
+# =============================================================
+#    ANNEX  G       GENERAL RECOMMENDATIONS FOR STEELWORK TENDERS AND CONTRACTS
+# =============================================================
+#    ANNEX  H       PLASTIC PROPERTIES OF BEAMS
+# =============================================================
+#------------------END------------------
+
+
+
+
+@staticmethod
     def compute_min_weld_thickness(part1_thickness, part2_thickness):
         thicker_part_thickness = max(part1_thickness, part2_thickness)
 

a6a2a7ac87a5a14e9822a8450b005f6c1277473d
Ajmal Babu M S <ajmalbabums@gmail.com>
2018-12-13 11:38:56 +0530

    refactor: Constructor for shear connection inputs

--- app/utils/common/input.py
+++ app/utils/common/input.py
@@ -11,7 +11,13 @@ class ConnectionInput(Input):
 
 class ShearConnectionInput(ConnectionInput):
 
-    def __init__(self):
+    def __init__(self, connectivity, supporting_member_section, supported_member_section, material):
+        self.connectivity = connectivity
+        if connectivity == "column_flange_beam_web" or "column_web_beam_web":
+            self.supporting_member = Column(supporting_member_section, material)
+        elif connectivity == "beam_beam":
+            self.supporting_member = Beam(supporting_member_section, material)
+        self.supported_member = Beam(supported_member_section, material)
         self.bolt = Bolt()
         self.bolt_diameter_list = []
         self.weld = Weld()
@@ -21,32 +27,30 @@ class ShearConnectionInput(ConnectionInput):
 class FinPlateConnectionInput(ShearConnectionInput):
 
     def __init__(self, connectivity, supporting_member_section, supported_member_section, material):
-        self.connectivity = connectivity
-        if connectivity == "column_flange_beam_web" or "column_web_beam_web":
-            self.supporting_member = Column(supporting_member_section, material)
-        elif connectivity == "beam_beam":
-            self.supporting_member = Beam(supporting_member_section, material)
-        self.supported_member = Beam(supported_member_section, material)
         self.plate = Plate()
-        super(FinPlateConnectionInput, self).__init__()
+        super(FinPlateConnectionInput, self).__init__(connectivity, supporting_member_section, supported_member_section,
+                                                      material)
 
 
 class EndPlateConnectionInput(ShearConnectionInput):
 
-    def __init__(self):
+    def __init__(self, connectivity, supporting_member_section, supported_member_section, material):
         self.plate = Plate()
-        super(EndPlateConnectionInput, self).__init__()
+        super(EndPlateConnectionInput, self).__init__(connectivity, supporting_member_section, supported_member_section,
+                                                      material)
 
 
 class CleatAngleConnectionInput(ShearConnectionInput):
 
-    def __init__(self):
+    def __init__(self, connectivity, supporting_member_section, supported_member_section, material):
         self.angle = Angle()
-        super(CleatAngleConnectionInput, self).__init__()
+        super(CleatAngleConnectionInput, self).__init__(connectivity, supporting_member_section,
+                                                        supported_member_section, material)
 
 
 class SeatedAngleConnectionInput(ShearConnectionInput):
 
-    def __init__(self):
+    def __init__(self, connectivity, supporting_member_section, supported_member_section, material):
         self.angle = Angle()
-        super(SeatedAngleConnectionInput, self).__init__()
+        super(SeatedAngleConnectionInput, self).__init__(connectivity, supporting_member_section,
+                                                         supported_member_section, material)

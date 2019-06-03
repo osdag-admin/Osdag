97d1aead78f3e76a50e13e63996bfaf844267c70
Aryamaan <aryamaan98@outlook.com>
2019-06-03 14:06:51 +0530

    Ui and backend changes for weld details done

--- Connections/Moment/ExtendedEndPlate/bbExtendedEndPlateSpliceCalc.py
+++ Connections/Moment/ExtendedEndPlate/bbExtendedEndPlateSpliceCalc.py
@@ -25,7 +25,6 @@ import logging
 flag = 1
 logger = None
 
-
 def module_setup():
     global logger
     logger = logging.getLogger("osdag.bbExtendedEndPlateSpliceCalc")
@@ -359,8 +358,9 @@ def bbExtendedEndPlateSplice(uiObj):
     # Read input values from Beam database
 
     dictbeamdata = get_beamdata(beam_sec)
-
+    global beam_tw
     beam_tw = float(dictbeamdata["tw"])
+    global beam_tf
     beam_tf = float(dictbeamdata["T"])
     beam_d = float(dictbeamdata["D"])
     beam_B = float(dictbeamdata["B"])

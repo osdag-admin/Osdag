3a2f842db5b36b28de4268d5529bbeacc0deddd2
Swaroop <swaroopanand26263@gmail.com>
2019-06-07 16:23:07 +0530

    made some changes

--- Connections/Moment/BCEndPlate/bc_endplate_main.py
+++ Connections/Moment/BCEndPlate/bc_endplate_main.py
@@ -1925,9 +1925,11 @@ class Maincontroller(QMainWindow):
 
         # nutSpace = 2 * float(outputobj["Plate"]["Thickness"]) + nut_T   # Space between bolt head and nut
         if conn_type == 'col_flange_connectivity':
-            nutSpace = float(column_data["T"]) + float(outputobj["Plate"]["Thickness"]) + nut_T #/ 2 + bolt_T / 2  # Space between bolt head and nut
+            nutSpace = float(column_data["T"]) + float(
+                outputobj["Plate"]["Thickness"]) + nut_T / 2 + bolt_T / 2  # Space between bolt head and nut
         else:
-            nutSpace = float(column_data["tw"]) + float(outputobj["Plate"]["Thickness"]) + nut_T #/ 2 + bolt_T / 2  # Space between bolt head and nut
+            nutSpace = float(column_data["tw"]) + float(
+                outputobj["Plate"]["Thickness"]) + nut_T / 2 + bolt_T / 2  # Space between bolt head and nut
 
         bbNutBoltArray = NutBoltArray(alist, beam_data, outputobj, nut, bolt, numberOfBolts, nutSpace, endplate_type)
 

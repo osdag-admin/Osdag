8a3998dce8b38b55d75c998a4608662e956280aa
origin <lokhandeyash004@gmail.com>
2020-03-04 12:01:49 +0530

    commit for 3d

--- design_type/connection/beam_cover_plate.py
+++ design_type/connection/beam_cover_plate.py
@@ -559,7 +559,9 @@ class BeamCoverPlate(MomentConnection):
         self.web_bolt.bolt_capacity = 69923.63636363638
         # self.web_bolt.min_edge_dist_round = 33
         # self.web_bolt.min_end_dist_round = 33
+
         # self.web_bolt.min_gauge_round = 50
+        #anjali
         # self.web_bolt.min_pitch_round = 50
 
         # self.web_bolt.max_edge_dist_round = 150

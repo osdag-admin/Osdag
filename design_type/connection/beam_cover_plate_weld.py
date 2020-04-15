f6ddb666c5810bb9d276037b22dc52f876859264
d33pthi <rdeepthireddy94@gmail.com>
2020-04-15 13:53:39 +0530

    merged

--- design_type/connection/beam_cover_plate_weld.py
+++ design_type/connection/beam_cover_plate_weld.py
@@ -1048,7 +1048,8 @@ class BeamCoverPlateWeld(MomentConnection):
                 self.l_req_flangelength = round_up(
                     ((2 * self.available_long_flange_length) + self.flange_plate.height + (
                             2 * self.flange_weld.size)), 5)
-                self.flange_weld.stress = self.flange_force / self.l_req_flange                if self.flange_weld.stress < self.flange_weld.strength:
+                self.flange_weld.stress = self.flange_force / self.l_req_flange
+                if self.flange_weld.stress < self.flange_weld.strength:
                     if self.available_long_flange_length > self.flange_plate.height:
                         self.design_status = True
                         break

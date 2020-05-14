a3f9970f190d1362237b73afcfadd273f6e549bc
AnandSwaroop <swaroopanand26263@gmail.com>
2020-05-14 19:45:50 +0530

    Edited tension welded cad model

--- cad/Tension/WeldedCAD.py
+++ cad/Tension/WeldedCAD.py
@@ -38,15 +38,13 @@ class TensionCAD(object):
         self.createPlateGeometry()
         self.createWeldGeometry()
 
-        self.member1_Model = self.member1.create_model()
-        self.plate1_Model = self.plate1.create_model()
-        self.plate2_Model = self.plate2.create_model()
-
     def createMemberGeometry(self):
         member1OriginL = numpy.array([0.0, 0.0, 0.0])
         member1_uDir = numpy.array([0.0, -1.0, 0.0])
         member1_wDir = numpy.array([1.0, 0.0, 0.0])
         self.member1.place(member1OriginL, member1_uDir, member1_wDir)
+
+        self.member1_Model = self.member1.create_model()
 
     def createPlateGeometry(self):
         plate1OriginL = numpy.array([0.0, 0.0, 0.0])
@@ -54,19 +52,25 @@ class TensionCAD(object):
         plate1_wDir = numpy.array([1.0, 0.0, 0.0])
         self.plate1.place(plate1OriginL, plate1_uDir, plate1_wDir)
 
+        self.plate1_Model = self.plate1.create_model()
+
         plate2OriginL = numpy.array([0.0, 0.0, 0.0])
         plate2_uDir = numpy.array([0.0, 1.0, 0.0])
         plate2_wDir = numpy.array([1.0, 0.0, 0.0])
         self.plate2.place(plate2OriginL, plate2_uDir, plate2_wDir)
 
+        self.plate2_Model = self.plate2.create_model()
+
     def createWeldGeometry(self):
         pass
 
     def get_members_models(self):
-        pass
+        member = self.member1_Model
+        return member
 
     def get_plates_models(self):
-        pass
+        plate = BRepAlgoAPI_Fuse(self.plate1_Model, self.plate2_Model).Shape()
+        return plate
 
     def get_welded_models(self):
         pass
@@ -75,10 +79,12 @@ class TensionCAD(object):
         pass
 
 if __name__ == '__main__':
-
+    import math
     from cad.items.plate import Plate
     from cad.items.filletweld import FilletWeld
     from cad.items.channel import Channel
+    from cad.items.angle import Angle
+    from cad.items.stiffener_plate import StiffenerPlate
 
     import OCC.Core.V3d
     from OCC.Core.Quantity import Quantity_NOC_SADDLEBROWN, Quantity_NOC_BLUE1
@@ -88,12 +94,31 @@ if __name__ == '__main__':
 
     from OCC.gp import gp_Pnt
     from OCC.Display.SimpleGui import init_display
+
     display, start_display, add_menu, add_function_to_menu = init_display()
 
-    member = Channel(B= , T= , D= , t= , R1= 0, R2= 0, L= 100)
-    plate = Plate(L= , W= , T= )
-    inline_weld = FilletWeld(b= ,h= ,L= )
-    opline_weld = FilletWeld(b= ,h= ,L= )
+    # member = Channel(B= , T= , D= , t= , R1= 0, R2= 0, L= 100)
+    member = Angle(L=2000, A=75, B=75, T=5, R1=0, R2=0)
+    # plate = Plate(L= , W= , T= )
+    plate_h = member.A + 30
+    plate_L = 100
+    plate_H = plate_h + 2 * (math.tan(30) * plate_L)
+    cut = 2 * (math.tan(30) * plate_L)
+    plate = StiffenerPlate(L=plate_H, W=plate_L, T=5, R11=plate_L, R12=cut, R21=plate_L, R22=cut, )
+    inline_weld = FilletWeld(b=0, h=0, L=0)
+    opline_weld = FilletWeld(b=0, h=0, L=0)
     member_data = 'Channel'
 
     tensionCAD = TensionCAD(member, plate, inline_weld, opline_weld, member_data)
+
+    tensionCAD.create_3DModel()
+    plate = tensionCAD.get_plates_models()
+    mem = tensionCAD.get_members_models()
+
+    Point = gp_Pnt(0.0, 0.0, 0.0)
+    display.DisplayMessage(Point, "Origin")
+
+    display.DisplayShape(mem, update=True)
+    display.DisplayColoredShape(plate, color='BLUE', update=True)
+
+    start_display()

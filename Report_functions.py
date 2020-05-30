9291711e583e6d0bcdebe90164465f3b138b5aef
priti73 <pritikumarinits@gmail.com>
2020-05-30 12:04:21 +0530

    update in report function

--- Report_functions.py
+++ Report_functions.py
@@ -383,6 +383,18 @@ def max_plate_ht_req(connectivity,beam_depth, beam_f_t, beam_r_r, notch, max_pla
     max_plate_ht_eqn.append(NoEscape(r'&=' + max_plate_h + '\end{aligned}'))
     return max_plate_ht_eqn
 
+def end_plate_ht_req(D,e,h_p):
+    D = str(D)
+    h_p = str(h_p)
+    e = str(e)
+    end_plate_ht_eqn = Math(inline=True)
+
+    end_plate_ht_eqn.append(NoEscape(r'\begin{aligned} &D + 4*e \\'))
+    end_plate_ht_eqn.append(NoEscape(r'&=' + D + '+' + ' 4*' + e + r'\\'))
+    end_plate_ht_eqn.append(NoEscape(r'&=' + h_p + '\end{aligned}'))
+    return end_plate_ht_eqn
+
+
 def min_plate_length_req(min_pitch, min_end_dist,bolt_line,min_length):
     min_pitch = str(min_pitch)
     min_end_dist = str(min_end_dist)

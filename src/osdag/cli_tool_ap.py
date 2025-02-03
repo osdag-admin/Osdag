7eb0ee09574ea0c6ebadfa44a88bd63971852216
aumghelani <shyam.jgd@gmail.com>
2025-02-03 08:53:48 +0530

    Updated argparse code

--- src/osdag/cli_tool_ap.py
+++ src/osdag/cli_tool_ap.py
@@ -1,3 +1,8 @@
 import argparse
 
-def
+parser = argparse.ArgumentParser(description='converts the input into a report')
+parser.add_argument('filename', help='The name of the file which should be processed')
+arguments = parser.parse_args()
+
+print(arguments)
+print(arguments.filename)

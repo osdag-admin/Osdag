b053c995aeae6187802bd1b46e25616eaac21dce
lakshanashreee <lakshanashree.s.2023.cse@ritchennai.edu.in>
2025-05-21 19:59:05 +0530

    Included marker @pytest.mark.xfail
    @pytest.mark.xfail is a pytest marker used to indicate that a particular test is expected to fail.

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -40,6 +40,7 @@ def test_tension_bolted_test3():
     run_test("TensionBoltedTest3.osi", expected_result)
 
 # Test function 4
+@pytest.mark.xfail
 def test_tension_bolted_test4():
     expected_result = {
         "Member.Designation": "Pass",

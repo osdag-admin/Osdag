38fd5c494c9002be367108e48d99508786ca6501
aumghelani <shyam.jgd@gmail.com>
2025-06-12 17:44:56 +0530

    Connected CLI to Main Software

--- src/osdag/setup.py
+++ src/osdag/setup.py
@@ -1,17 +1,20 @@
 # setup.py
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='osdag-cli',
     version='0.1.0',
-    py_modules=['osdag_cli'],
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'Click',
+        'PyYAML',
+        'PyQt5',
     ],
     entry_points={
         'console_scripts': [
-            'osdag-cli = osdag_cli:determine_module',
+            'osdag-cli = osdag.cli_shell:cli',
         ],
     },
 )

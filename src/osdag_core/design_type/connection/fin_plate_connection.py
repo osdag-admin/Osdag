07a88fbc763f6fa37d899e892933c823993340ae
mhsuhail00 <mhsuhail00@gmail.com>
2025-10-03 22:39:19 +0530

    Hover over finplate cad fix

--- src/osdag_core/design_type/connection/fin_plate_connection.py
+++ src/osdag_core/design_type/connection/fin_plate_connection.py
@@ -439,11 +439,11 @@ class FinPlateConnection(ShearConnection):
         out_list.append(t16)
 
         # Populate hover dict
-        self.hover_dict["Bolt"] = f"Grade: {self.bolt.bolt_grade_provided if flag else ''}<br>Diameter: {int(self.bolt.bolt_diameter_provided) if flag else ''} mm<br>No. of Bolts: {int(self.plate.bolts_one_line)*int(self.plate.bolt_line) if flag else ''}"
+        self.hover_dict["Bolt"] = f"<b>Bolt</b><br>Grade: {self.bolt.bolt_grade_provided if flag else ''}<br>Diameter: {int(self.bolt.bolt_diameter_provided) if flag else ''} mm<br>No. of Bolts: {int(self.plate.bolts_one_line)*int(self.plate.bolt_line) if flag else ''}"
         
-        self.hover_dict["Plate"]= f"Plate: {float(self.plate.length) if flag else ''} x {float(self.plate.height) if flag else ''} x {self.plate.thickness_provided if flag else ''}"
+        self.hover_dict["Plate"]= f"Plate: {float(self.plate.length) if flag else ''} mm x {float(self.plate.height) if flag else ''} mm x {self.plate.thickness_provided if flag else ''} mm"
             
-        self.hover_dict["Weld"]= f"Weld: {self.weld.size if flag else ''} mm"
+        self.hover_dict["Weld"]= f"<b>Weld</b><br>Size: {self.weld.size if flag else ''} mm<br>Length: {self.plate.height if flag else ''} mm"
 
         return out_list
 

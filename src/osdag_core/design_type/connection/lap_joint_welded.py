42e1c9efb78428dbb5f5957e8a6b79f5e57afcb4
mhsuhail00 <mhsuhail00@gmail.com>
2026-01-13 04:15:56 +0530

    Removed Length in hover of Simple Conn Modules

--- src/osdag_core/design_type/connection/lap_joint_welded.py
+++ src/osdag_core/design_type/connection/lap_joint_welded.py
@@ -292,13 +292,11 @@ class LapJointWelded(MomentConnection):
 
         self.hover_dict["Plate 1"] = (
             f"<b>Plate 1</b><br>"
-            f"Length: {round(float(self.plate1.length), 2) if flag and self.plate1.length else ''} mm<br>"
             f"Width: {round(float(self.plate1.height), 2) if flag and self.plate1.height else ''} mm<br>"
             f"Thickness: {round(float(self.plate1.thickness_provided), 2) if flag and self.plate1.thickness_provided else ''} mm"
         )
         self.hover_dict["Plate 2"] = (
             f"<b>Plate 2</b><br>"
-            f"Length: {round(float(self.plate2.length), 2) if flag and self.plate2.length else ''} mm<br>"
             f"Width: {round(float(self.plate2.height), 2) if flag and self.plate2.height else ''} mm<br>"
             f"Thickness: {round(float(self.plate2.thickness_provided), 2) if flag and self.plate2.thickness_provided else ''} mm"
         )

656e09705d762a842764e7dcf2a25ac81e01d12f
mhsuhail00 <mhsuhail00@gmail.com>
2026-01-21 01:54:54 +0530

    Reconnected Import Osi on Home Page

--- src/osdag_gui/ui/windows/home_window.py
+++ src/osdag_gui/ui/windows/home_window.py
@@ -241,8 +241,8 @@ class HomeWindow(QWidget):
                 button = TopButton(black_icon, white_icon, label)
 
             if label.strip() == "Import":
-                #button.clicked.connect(lambda checked=False: self.triggerLoadOsi.emit())
-                button.clicked.connect(lambda checked=False: print(f"@Total: {len(QApplication.allWidgets())}"))
+                button.clicked.connect(lambda checked=False: self.triggerLoadOsi.emit())
+                # button.clicked.connect(lambda checked=False: print(f"@Total: {len(QApplication.allWidgets())}"))
 
             self.buttons.append(button)
             self.button_group.addButton(button, i) # Add button to the group with an ID

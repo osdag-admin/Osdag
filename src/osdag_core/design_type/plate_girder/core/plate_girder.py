d8511b987adae27d0a970a5bca6b15d0e4589ddc
nishikantmandal007 <nishikantmandal007@gmail.com>
2026-01-23 16:52:03 +0530

    new popUI for additional inputs

--- src/osdag_core/design_type/plate_girder/core/plate_girder.py
+++ src/osdag_core/design_type/plate_girder/core/plate_girder.py
@@ -18,7 +18,8 @@ from ....utils.common.Unsymmetrical_Section_Properties import Unsymmetrical_I_Se
 
 # New imports
 from ....Common import *
-from ..gui.dialogs import RangeInputDialog, PopupDialog
+from ..gui.dialogs import RangeInputDialog
+from osdag_gui.ui.components.dialogs.customized_popup import CustomValueSelectPopup
 from ..gui.widgets import My_ListWidget, My_ListWidgetItem
 from .section import Section, calc_yj, shear_stress_unsym_I, classify_section
 from .pso_optimizer import GlobalBestPSO
@@ -343,50 +344,42 @@ class PlateGirderWelded(Member):
         if arg[0] == 'All':
             return {KEY_IntermediateStiffener_thickness_val : VALUES_STIFFENER_THICKNESS}
         else:
-            popup = PopupDialog()
-            popup.listWidget.addItems(VALUES_STIFFENER_THICKNESS)
+            # Use new styled popup from osdag_gui
+            window = QDialog()
+            ui = CustomValueSelectPopup()
+            ui.setupUi(window, [], "")  # No disabled values, no note
             
             # Pre-select previously selected items, or all items if first time
-            if PlateGirderWelded.int_thicklist and len(PlateGirderWelded.int_thicklist) > 0:
-                # Restore previous selections
-                for item_text in PlateGirderWelded.int_thicklist:
-                    items = popup.listWidget.findItems(item_text, Qt.MatchExactly)
-                    if items:
-                        popup.listWidget_2.addItem(item_text)
-                        popup.listWidget.takeItem(popup.listWidget.row(items[0]))
-            else:
-                # First time: move all items to Selected by default
-                popup.move_all_to_selected()
+            existing_selections = PlateGirderWelded.int_thicklist if PlateGirderWelded.int_thicklist else VALUES_STIFFENER_THICKNESS
+            ui.addAvailableItems(VALUES_STIFFENER_THICKNESS, existing_selections)
             
-            if popup.exec_() == QDialog.Accepted:
-                selected_items = popup.get_selected_items()
-            PlateGirderWelded.int_thicklist = selected_items
-            return {KEY_IntermediateStiffener_thickness_val : selected_items}                                 
+            window.exec()
+            selected_items = ui.get_right_elements()
+            
+            if selected_items:
+                PlateGirderWelded.int_thicklist = selected_items
+            return {KEY_IntermediateStiffener_thickness_val : selected_items if selected_items else VALUES_STIFFENER_THICKNESS}                                 
             
     def Long_stiffener_thickness_customized(self, arg):
-        selected_items2 = []
+        selected_items = []
         if arg[0] == 'All':
             return {KEY_LongitudnalStiffener_thickness_val : VALUES_STIFFENER_THICKNESS}
         else:
-            popup = PopupDialog()
-            popup.listWidget.addItems(VALUES_STIFFENER_THICKNESS)
+            # Use new styled popup from osdag_gui
+            window = QDialog()
+            ui = CustomValueSelectPopup()
+            ui.setupUi(window, [], "")  # No disabled values, no note
             
             # Pre-select previously selected items, or all items if first time
-            if PlateGirderWelded.long_thicklist and len(PlateGirderWelded.long_thicklist) > 0:
-                # Restore previous selections
-                for item_text in PlateGirderWelded.long_thicklist:
-                    items = popup.listWidget.findItems(item_text, Qt.MatchExactly)
-                    if items:
-                        popup.listWidget_2.addItem(item_text)
-                        popup.listWidget.takeItem(popup.listWidget.row(items[0]))
-            else:
-                # First time: move all items to Selected by default
-                popup.move_all_to_selected()
+            existing_selections = PlateGirderWelded.long_thicklist if PlateGirderWelded.long_thicklist else VALUES_STIFFENER_THICKNESS
+            ui.addAvailableItems(VALUES_STIFFENER_THICKNESS, existing_selections)
             
-            if popup.exec_() == QDialog.Accepted:
-                selected_items2 = popup.get_selected_items()
-            PlateGirderWelded.long_thicklist = selected_items2
-            return {KEY_LongitudnalStiffener_thickness_val : selected_items2}
+            window.exec()
+            selected_items = ui.get_right_elements()
+            
+            if selected_items:
+                PlateGirderWelded.long_thicklist = selected_items
+            return {KEY_LongitudnalStiffener_thickness_val : selected_items if selected_items else VALUES_STIFFENER_THICKNESS}
 
 
     @staticmethod

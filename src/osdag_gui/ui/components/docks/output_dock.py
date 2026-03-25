eabef4c78870c5e5c5ee2b949d11b20f143153ca
Ayushman Dwivedi <dwivediayushman1@gmail.com>
2026-03-26 01:45:58 +0530

    Updated fin plate and end plate capacity , Block shear and tension logic

--- src/osdag_gui/ui/components/docks/output_dock.py
+++ src/osdag_gui/ui/components/docks/output_dock.py
@@ -44,7 +44,7 @@ from osdag_gui.ui.components.output_details.c2cEndPlate import C2CEndPlateDetail
 from osdag_gui.ui.components.output_details.finPlateCapacity import (
     FinPlateCapacityDetails, 
     SectionCapacityDetails,
-) 
+)
 from osdag_gui.ui.components.output_details.endPlateCapacity import (
     EndPlateCapacityDetails,      
     EndPlateSectionDetails,       
@@ -53,6 +53,7 @@ from osdag_gui.ui.components.output_details.seatedAngleCapacity import (
     SeatedAngleCapacityDetails,
     SeatedAngleSectionDetails,
 )
+
 from osdag_gui.ui.components.output_details.endPlate import EndPlateDetails
 from osdag_gui.ui.components.output_details.boltPattern import BoltPatternGenerator
 from osdag_gui.ui.components.output_details.seatedAngleSpacing import SeatedAngleDetails
@@ -687,7 +688,6 @@ class OutputDock(QWidget):
 
     def output_button_connect(self, spacing_button_list, button):
         button.clicked.connect(lambda: self.spacing_dialog(self.backend, spacing_button_list, button))
-
     def spacing_dialog(self, main, button_list, button):
         for op in button_list:
             tup = op[3]

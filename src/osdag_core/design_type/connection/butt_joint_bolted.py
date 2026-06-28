b5ae173bcbeab519a297367e6255230d8c48f6a8
smansipillai90 <smansipillai90@gmail.com>
2026-06-28 15:31:32 +0530

    Fix : Corrected issues 16,17,19

--- src/osdag_core/design_type/connection/butt_joint_bolted.py
+++ src/osdag_core/design_type/connection/butt_joint_bolted.py
@@ -1732,8 +1732,6 @@ class ButtJointBolted(MomentConnection):
                 "SubSection", "Bolt Arrangement", "|p{4cm}|p{5cm}|p{5.5cm}|p{1.5cm}|"
             ])
 
-            int cols
-            str col_text
 
             if cols == 1:
                 col_text = "column"
@@ -1767,11 +1765,11 @@ class ButtJointBolted(MomentConnection):
                 yield_req = Math(inline=True)
                 yield_req.append(NoEscape(r'\begin{aligned}\\'))
 
-                Tdg_kn = round(self.section.yield_req / 1000, 2) #converting to kn before displaying
+                #Tdg_kn = round(self.section.yield_req / 1000, 2) #converting to kn before displaying
                 yield_req.append(NoEscape(r'T_{dg} &= \frac{A_g \cdot f_y}{\gamma_{m0}}\\\\'))
-                yield_req.append(NoEscape(r'&=' + str(Tdg_kn) + r' \text{ kN}'))
-                #yield_req.append(NoEscape(r'&= \frac{' + str(A_g) + r' \times ' + str(fy) + r'}{1.10}\\\\'))
-                #yield_req.append(NoEscape(r'&= ' + f'{T_dg:.2f}' + r' \text{ kN}\\'))
+                #yield_req.append(NoEscape(r'&=' + str(Tdg_kn) + r' \text{ kN}'))
+                yield_req.append(NoEscape(r'&= \frac{' + str(A_g) + r' \times ' + str(fy) + r'}{1.10}\\\\'))
+                yield_req.append(NoEscape(r'&= ' + f'{T_dg/1000:.2f}' + r' \text{ kN}\\'))
                 yield_req.append(NoEscape(r'&[\text{Ref. Cl. 6.2}]'))
                 yield_req.append(NoEscape(r'\end{aligned}'))
                 self.report_check.append(["Gross Section Yield", "", yield_req, ""])
@@ -1786,11 +1784,11 @@ class ButtJointBolted(MomentConnection):
                 
                 rup_req = Math(inline=True)
                 rup_req.append(NoEscape(r'\begin{aligned}\\'))
-                Tdn_kn = round(self.plate.rup_req/ 1000, 2)
+                #Tdn_kn = round(self.plate.rup_req/ 1000, 2)
                 rup_req.append(NoEscape(r'T_{dn} &= \frac{0.9 A_n f_u}{\gamma_{m1}}\\'))
                 rup_req.append(NoEscape(r'&=' + str(Tdn_kn) + r' \text{ kN}'))
-                #rup_req.append(NoEscape(r'&= \frac{0.9 \times ' + f'{An_disp:.2f}' + r' \times ' + str(fu) + r'}{1.25}\\'))
-                #rup_req.append(NoEscape(r'&= ' + f'{T_dn:.2f}' + r' \text{ kN}\\'))
+                rup_req.append(NoEscape(r'&= \frac{0.9 \times ' + f'{An_disp:.2f}' + r' \times ' + str(fu) + r'}{1.25}\\'))
+                rup_req.append(NoEscape(r'&= ' + f'{T_dn/1000:.2f}' + r' \text{ kN}\\'))
                 rup_req.append(NoEscape(r'&[\text{Ref. Cl. 6.3}]'))
                 rup_req.append(NoEscape(r'\end{aligned}'))
                 self.report_check.append(["Net Section Rupture", "", rup_req, ""])
